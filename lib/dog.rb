c94627d18495833848b9125d344919c99121bb97
Annie Bourke <aabourke@gmail.com>
2018-05-07 22:58:39 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+require_relative "../config/environment.rb"
+
+class Dog
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
