9fa3b7e4234c96ce1c2c16c94e00b50fdfd8de50
Annie Bourke <aabourke@gmail.com>
2018-05-07 23:31:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,8 +44,8 @@ class Dog
     self
   end
 
-  def self.create(name, breed)
-    dog = Dog.new(name, breed)
+  def self.create(name:, breed:)
+    dog = Dog.new(name:, breed:)
     dog.save
     dog
   end
